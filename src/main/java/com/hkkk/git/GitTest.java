3f75d5968e4eb6f178b9404f868ddbff8800b8e8
hkkk <2531578012@qq.com>
2023-06-15 21:32:56 +0800

    222-commit

--- src/main/java/com/hkkk/git/GitTest.java
+++ src/main/java/com/hkkk/git/GitTest.java
@@ -13,5 +13,6 @@ package com.hkkk.git;/**
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2222222222222");
     }
 }
