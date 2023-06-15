331afc220e1fcfee2a545ece5327e9e0c4896f1c
hkkk <2531578012@qq.com>
2023-06-15 21:31:05 +0800

    first-commit

---
+++ src/main/java/com/hkkk/git/GitTest.java
@@ -0,0 +1,17 @@
+package com.hkkk.git;/**
+ * @description:
+ * @author: hkkk
+ * @create 2023-06-15 21:26
+ */
+
+/**
+ * @Description:
+ * @Auther: HKKK
+ * @Date: 2023/6/15 21:26
+ * @Version 1.0.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
